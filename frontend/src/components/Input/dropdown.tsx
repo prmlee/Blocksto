c42bba2704ee044e738641d89986de83fc50bf89
prmlee <promotionlee.bs@gmail.com>
2022-11-13 08:23:24 -0800

    format the code

--- frontend/src/components/Input/dropdown.tsx
+++ frontend/src/components/Input/dropdown.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './index.css';
 
 type DropdownBasicProps = {
   label?: string;
   values: string[];
   onSelect: (index: number) => void;
-}
+};
 
-export const PrimaryDropdown = (props: DropdownBasicProps) => { 
+export const PrimaryDropdown = (props: DropdownBasicProps) => {
   const { label, values, onSelect } = props;
   const [isDown, setIsDown] = useState<boolean>(false);
   const [selected, setSelected] = useState<number>(0);
@@ -17,21 +17,28 @@ export const PrimaryDropdown = (props: DropdownBasicProps) => {
       <div className="dropdown-header">
         <span>{`${label} :`}</span>
         <span>{values[selected]}</span>
-        <img src="./images/svg/drop-arrow.svg" alt="Drop Icon" onClick={() => setIsDown(!isDown)} />
+        <img
+          src="./images/svg/drop-arrow.svg"
+          alt="Drop Icon"
+          onClick={() => setIsDown(!isDown)}
+        />
       </div>
       {isDown && (
         <div className="dropdown-content">
-          {
-            values.map((value, index) => ( 
-              <p key={`dropdown-${index}`} onClick={() => {
+          {values.map((value, index) => (
+            <p
+              key={`dropdown-${index}`}
+              onClick={() => {
                 setSelected(index);
                 setIsDown(false);
                 onSelect(index);
-              }}>{value}</p>
-            ))
-          }
+              }}
+            >
+              {value}
+            </p>
+          ))}
         </div>
       )}
     </div>
-  )
-}
+  );
+};
