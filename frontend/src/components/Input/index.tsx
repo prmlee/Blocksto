f76a13fd717e003ae30cc4db2a60de1af79d224e
prmlee <promotionlee.bs@gmail.com>
2022-11-13 08:23:24 -0800

    format the code

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './index.css'
+import './index.css';
 
 type InputBasicProps = {
   label?: string;
@@ -7,28 +7,46 @@ type InputBasicProps = {
   type: string;
   value: string | boolean;
   onChange: (value: string | boolean) => void;
-}
+};
 
 const defaultProps = {
   required: false,
   type: 'text',
-}
+};
 
-export const PrimaryInput = (props: InputBasicProps) => { 
+export const PrimaryInput = (props: InputBasicProps) => {
   const { label, required, type, value, onChange } = props;
 
-  const inputElements: {[key: string]: any} = {
-    'textarea': <textarea rows={4} value={value.toString()} onChange={(e) => onChange(e.target.value)} />,
-    'checkbox': <input type={type} checked={value.toString() === 'true' ? true : false} onChange={(e) => onChange(e.target.checked)} />,
-    'other': <input type={type} value={value.toString()} onChange={e => onChange(e.target.value)} />,
-  }
+  const inputElements: { [key: string]: any } = {
+    textarea: (
+      <textarea
+        rows={4}
+        value={value.toString()}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ),
+    checkbox: (
+      <input
+        type={type}
+        checked={value.toString() === 'true' ? true : false}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+    ),
+    other: (
+      <input
+        type={type}
+        value={value.toString()}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ),
+  };
 
   return (
-    <div className={ `input-primary ${type === 'checkbox' ? 'w-content' : ''}` }>
+    <div className={`input-primary ${type === 'checkbox' ? 'w-content' : ''}`}>
       {label && <span>{label + (required ? '*' : '') + ' :'}</span>}
       {inputElements[type] ?? inputElements['other']}
     </div>
   );
-}
+};
 
 PrimaryInput.defaultProps = defaultProps;
