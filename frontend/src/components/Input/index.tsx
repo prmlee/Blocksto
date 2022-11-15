5b585f709ccde5e224f60cdda7d76623f3bec223
prmlee <promotionlee.bs@gmail.com>
2022-11-15 04:56:38 -0800

    add the normal input

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -49,13 +49,38 @@ export const PrimaryInput = (props: InputBasicProps) => {
 };
 
 export const NormalInput = (props: InputBasicProps) => {
-  const { label, required, type } = props;
+  const { label, required, type, value, onChange } = props;
+  const inputElements: { [key: string]: any } = {
+    textarea: (
+      <textarea
+        rows={4}
+        value={value.toString()}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ),
+    checkbox: (
+      <input
+        type={type}
+        checked={value.toString() === 'true' ? true : false}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+    ),
+    other: (
+      <input
+        type={type}
+        value={value.toString()}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ),
+  };
 
   return (
     <div className={`input-normal ${type === 'checkbox' ? 'w-content' : ''}`}>
       {label && <span>{label + (required ? '*' : '') + ' :'}</span>}
+      {inputElements[type] ?? inputElements['other']}
     </div>
   );
 };
 
 PrimaryInput.defaultProps = defaultProps;
+NormalInput.defaultProps = defaultProps;
